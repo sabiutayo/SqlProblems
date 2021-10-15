48808b46f20688ae68946fe2a371c52ec9af8767
Sabie George Ionut <sabiutayo@yahoo.com>
2021-10-15 07:49:31 +0300

    24

--- Problem 24.sql
+++ Problem 24.sql
@@ -1,3 +1,15 @@
+--My version
 SELECT customerid, companyname, region
 FROM customers c 
 ORDER BY region NOT NULL DESC, region;
+
+--Their version
+SELECT customerid, companyname, region
+FROM customers c 
+ORDER BY 
+CASE 
+	WHEN region IS NULL THEN 1
+	ELSE 0
+END
+,region, customerid
+	;
